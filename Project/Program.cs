b865f67e225860520b47e9c5a41ffcae09cd05bc
Dylan Francis <s215008243@nmmu.ac.za>
2019-05-06 22:28:10 +0200

    Initial commit

---
+++ Project/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Project {
+    internal class Program {
+        public static void Main(string[] args) {
+            Parser P = new Parser("a - ( b * c )");
+            Console.ReadLine();
+        }
+    }
+}
